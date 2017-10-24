708110eb8ebb5cda1a10fed0a1c7530b3f128ec0
Alexander Martinz <alex@amartinz.at>
2017-10-24 15:21:57 +0200

    cheeseburger: sepolicy: reenable device/qcom/common sepolicy
    Change-Id: I9c35272a165bd8005ff91b5d679510aecb4649e8
    Signed-off-by: Alexander Martinz <alex@amartinz.at>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -254,7 +254,7 @@ PROTOBUF_SUPPORTED := true
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
-BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy
+#BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy
 
 # Sensors
 USE_SENSOR_MULTI_HAL := true
