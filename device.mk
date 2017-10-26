1b42f524b01d67521757a822a197cde25823e8ed
villarleg <santialovera20@gmail.com>
2017-10-26 18:57:44 +0200

    cheeseburguer: Fix paths

--- device.mk
+++ device.mk
@@ -261,26 +261,26 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     power.msm8998
 
-# Prebuilds
+# Prebuilts
 PRODUCT_PACKAGES += \
     OnePlusCamera \
     OnePlusCameraService \
     OnePlusGallery
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/prebuild/lib/libcameraservice.so:system/lib/libcameraservice.so \
-    $(LOCAL_PATH)/prebuild/lib/libfilter-sdk.so:system/lib/libfilter-sdk.so \
-    $(LOCAL_PATH)/prebuild/lib/libopbaselib.so:system/lib/libopbaselib.so \
-    $(LOCAL_PATH)/prebuild/lib/libopcamera.so:system/lib/libopcamera.so \
-    $(LOCAL_PATH)/prebuild/lib/libopcameralib.so:system/lib/libopcameralib.so \
-    $(LOCAL_PATH)/prebuild/lib64/libcameraservice.so:system/lib64/libcameraservice.so \
-    $(LOCAL_PATH)/prebuild/lib64/libfilter-sdk.so:system/lib64/libfilter-sdk.so \
-    $(LOCAL_PATH)/prebuild/lib64/libopbaselib.so:system/lib64/libopbaselib.so \
-    $(LOCAL_PATH)/prebuild/lib64/libopcamera.so:system/lib64/libopcamera.so \
-    $(LOCAL_PATH)/prebuild/lib64/libopcameralib.so:system/lib64/libopcameralib.so
+    $(LOCAL_PATH)/prebuilt/lib/libcameraservice.so:system/lib/libcameraservice.so \
+    $(LOCAL_PATH)/prebuilt/lib/libfilter-sdk.so:system/lib/libfilter-sdk.so \
+    $(LOCAL_PATH)/prebuilt/lib/libopbaselib.so:system/lib/libopbaselib.so \
+    $(LOCAL_PATH)/prebuilt/lib/libopcamera.so:system/lib/libopcamera.so \
+    $(LOCAL_PATH)/prebuilt/lib/libopcameralib.so:system/lib/libopcameralib.so \
+    $(LOCAL_PATH)/prebuilt/lib64/libcameraservice.so:system/lib64/libcameraservice.so \
+    $(LOCAL_PATH)/prebuilt/lib64/libfilter-sdk.so:system/lib64/libfilter-sdk.so \
+    $(LOCAL_PATH)/prebuilt/lib64/libopbaselib.so:system/lib64/libopbaselib.so \
+    $(LOCAL_PATH)/prebuilt/lib64/libopcamera.so:system/lib64/libopcamera.so \
+    $(LOCAL_PATH)/prebuilt/lib64/libopcameralib.so:system/lib64/libopcameralib.so
 
-#    $(LOCAL_PATH)/prebuild/lib/libmediaplayerservice.so:system/lib/libmediaplayerservice.so \
-#    $(LOCAL_PATH)/prebuild/lib64/libmediaplayerservice.so:system/lib64/libmediaplayerservice.so \
+#    $(LOCAL_PATH)/prebuilt/lib/libmediaplayerservice.so:system/lib/libmediaplayerservice.so \
+#    $(LOCAL_PATH)/prebuilt/lib64/libmediaplayerservice.so:system/lib64/libmediaplayerservice.so \
 
 # QMI
 PRODUCT_PACKAGES += \
