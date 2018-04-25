0bd225b8f81cbefa29e5ef1b2b0069c48511760e
villarleg <santialovera20@gmail.com>
2018-04-25 21:40:55 +0200

    Cheeseburger: Uncommonize kernel and track flar2 kernel
    Change-Id: Id6ca73ced390151beb44b732536278a1bcfde541

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -42,5 +42,20 @@ BOARD_HARDWARE_CLASS += $(DEVICE_PATH)/lineagehw
 
 ALLOW_MISSING_DEPENDENCIES=true
 
+# Kernel
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1 service_locator.enable=1 swiotlb=2048
+BOARD_KERNEL_CMDLINE += androidboot.configfs=true
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
+BOARD_RAMDISK_OFFSET     := 0x02000000
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+TARGET_COMPILE_WITH_MSM_KERNEL := true
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
+TARGET_KERNEL_SOURCE := kernel/oneplus/msm8998
+TARGET_KERNEL_CONFIG := elementalx_defconfig
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+
 # inherit from the proprietary version
 -include vendor/oneplus/cheeseburger/BoardConfigVendor.mk
